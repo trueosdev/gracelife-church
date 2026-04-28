00b02b2119929f0aff698b0735760aace31dd31e
jack :) <trueos.dev@gmail.com>
2026-04-28 10:32:59 -0500

    Routine updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {},
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Exclude canvas and other Node.js modules from client bundle
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
         net: false,
         tls: false,
       };
-      
+
       // Ignore pdfjs-dist worker files during bundling
       config.module.rules.push({
         test: /pdf\.worker\.(min\.)?js/,
@@ -27,13 +27,13 @@ const nextConfig: NextConfig = {
           filename: "static/worker/[hash][ext][query]",
         },
       });
-      
+
       // Handle pdfjs-dist module resolution
       config.resolve.alias = {
         ...config.resolve.alias,
         canvas: false,
       };
-      
+
       // Ignore warnings from pdfjs-dist
       config.ignoreWarnings = [
         ...(config.ignoreWarnings || []),
@@ -44,4 +44,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
